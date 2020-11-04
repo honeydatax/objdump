543a72c9e0985ff818751149b98598efd4ffb15c
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-11-04 19:22:52 +0000

    arrays debug

---
+++ debug.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+ void putst(int a){
+	 printf("%d\n",a) ;
+ }
---
+++ debugt.c
@@ -0,0 +1,9 @@
+extern void putst(int a);
+
+int main(int argc,char *argv[]){
+	int i=0;
+	for(i=0;i<16;i++){
+		putst(i);
+	}
+	return 0;
+} 
